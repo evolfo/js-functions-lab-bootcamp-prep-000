160a4364b0b3f06f73f1fde83ad3ec8034005652
ANGELO SPAMPINATO <spampinatodesign@gmail.com>
2018-11-30 01:22:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // write your code below!
+<<<<<<< HEAD
 function happyHolidays(string) {
 	return "Happy holidays!"
 }
@@ -13,4 +14,13 @@ function happyHolidayTo(holiday, name) {
 
 function holidayCountdown(holiday, days) {
 	return `It's ${days} days until ${holiday}!`
+=======
+
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo (holiday, name) {
+  return `Happy ${holiday}, ${name}`
+>>>>>>> aab6e14e02605e027b2edddcb45f81b01eb86ca0
 }
